22c41ee10a7276517165769e52946aa572563ab2
Marouane-hourma <148641307+Marouane-hourma@users.noreply.github.com>
2023-12-02 14:08:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import os
 
 import tensorflow as tf
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-UPLOAD_FOLDER = "uploads"
 STATIC_FOLDER = "static"
 
 # Load model
@@ -53,17 +51,12 @@ def home():
 
 @app.route("/classify", methods=["POST", "GET"])
 def upload_file():
-
     if request.method == "GET":
         return render_template("home.html")
-
     else:
         file = request.files["image"]
-        upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        print(upload_image_path)
-        file.save(upload_image_path)
 
-        label, prob = classify(cnn_model, upload_image_path)
+        label, prob = classify(cnn_model, file)
 
         prob = round((prob * 100), 2)
 
@@ -72,12 +65,8 @@ def upload_file():
     )
 
 
-@app.route("/classify/<filename>")
-def send_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
-
-
 if __name__ == "__main__":
     app.debug = True
     app.run(debug=True)
     app.debug = True
+
